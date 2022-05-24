a9eeb9c09f028d44321c17321abefb0ba50971db
Emma Duane <emmakduane@gmail.com>
2022-05-23 19:33:34 -0700

    Updates

---
+++ precache-manifest.52d7231d8ac05072899a96463f8094fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c752bc9d146bb8db9457",
+    "url": "/favorite-classes/static/css/main.7e9905c0.chunk.css"
+  },
+  {
+    "revision": "c752bc9d146bb8db9457",
+    "url": "/favorite-classes/static/js/main.c752bc9d.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "/favorite-classes/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "a9b3ea059a7d1c04f463",
+    "url": "/favorite-classes/static/js/2.a9b3ea05.chunk.js"
+  },
+  {
+    "revision": "ea367f3561aa1a33c677",
+    "url": "/favorite-classes/static/js/runtime~main.ea367f35.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/favorite-classes/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "ecff82971a5ca594773dcd6b72d0e9ec",
+    "url": "/favorite-classes/static/media/ucibackground.ecff8297.png"
+  },
+  {
+    "revision": "af32ed15aae766e9432a5dd5594df92c",
+    "url": "/favorite-classes/index.html"
+  }
+];
